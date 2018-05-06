56bec8cd0d75b0f29c5319dfebfaf875155bbc79
OLGA\Ольга <dprily@gmail.com>
2018-05-06 11:19:24 +0300

    Registration routers

---
+++ src/components/Ads/ListAd.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <h1>ListAd</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+    export default {
+        name: "ListAd"
+    }
+</script>
+
+<style scoped>
+
+</style>
