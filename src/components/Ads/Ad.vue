ede33281c440e426a321b1f422e9a264d90e7040
OLGA\Ольга <dprily@gmail.com>
2018-05-09 17:33:27 +0300

    Create Ad page

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,23 @@
   <v-container>
     <v-layout>
       <v-flex xs12>
-        <h1>Ad</h1>
+        <v-card>
+            <v-card-media
+              src="/static/img/image-1.png"
+              height="200px">
+            </v-card-media>
+            <v-card-text>
+              <div>
+                <h2 class="text--primary">First ad</h2>
+                <p>Hello i am description</p>
+              </div>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat class="warning">Edit</v-btn>
+              <v-btn class="success">Buy</v-btn>
+            </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
